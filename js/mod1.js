db0b2fcc4d363750dfb1116584b43ae3067179c8
Iryna Ielkina <ielkinairinka@gmail.com>
2023-06-23 18:52:34 +0300

    mod1

---
+++ js/mod1.js
@@ -0,0 +1,190 @@
+/*Задача */
+//Написать сценарий который будет выводить среднее количество символов 3 введеных строк
+//пользователь будет вводить строки в prompt
+//а результат будет выводить с помощью alert
+
+// const textFromUserFirst = prompt('ВВедите первую строку');
+// const textFromUserSecond = prompt('ВВедите вторую строку');
+// const textFromUserFired = prompt('ВВедите третью строку');
+// const textFull = textFromUserFirst + textFromUserSecond + textFromUserFired;
+// let textSum = Math.round(textFull.length / 3);
+
+// alert(textSum);
+
+/*Задача*/
+
+//Написать сценарий который будет у пользователя спрашивать 2 числа и вывести делятся ли они без остатка друг на друга
+
+// const firsNumber = 10;//prompt('Введите первое число')
+// const secondNumber = 2;//prompt('Введите второе число')
+// const result = firsNumber % secondNumber || `делиться без остатка`;
+// const result2 = firsNumber % secondNumber && `делиться c остатка`;
+// // ` использовать только тогда когда будем использовать интерполяцию потому как они требуют
+// //доп ресурса из за поддержки интерполяции
+// console.log(result);
+// console.log(result2);
+
+/*Задача*/
+
+//Спросить у пользователя две строчки и вывести строку и его длину
+// const firstStr = 'Миру'   //prompt('Введите первое слово')
+// const secondStr = 'Мир'    //prompt('Введите второе слово')
+// const result = `${firstStr} ${secondStr} длина фразы ${(firstStr + secondStr).length} символов`
+// console.log(result);
+
+/*Задача */
+//Напиши сценарий который будет спрашивать у пользователя 2 слова и выводи сумму их длинны без учета пробелов
+// const firstUserStr = 'dfg'
+// const secondUserStr = 'df'
+// const fullText = `${firstUserStr}${secondUserStr}`
+// const result = fullText.replaceAll(' ', '')//убирает все пробелы
+// console.log(fullText.length);
+
+/*Задача */
+//Напиши сценарий который будет выводить количество символов введеного строки
+//пользователь будет вводить строки в prompt
+//а результат будет выводить с помощью alert
+//если пользователь написал пустую строку выводит нет значения
+
+// const textFromUser = 'лол'; //prompt('ВВедите строку');
+// const result = textFromUser.length || 'нет значения'
+// console.log(result);
+
+/*Задача*/
+
+//Написать сценарий который будет парное ли число
+//если да выводите с помощью алерт  да парное
+//если нет то не парное
+
+//1 получить число от пользователя
+//2 проверить парное или нет
+//3 вывести да или нет
+
+// const samNum = +prompt('введите число')
+
+// const result = samNum % 2 || 'парное'
+
+// console.log(result);
+
+/*Задача */
+
+//Спроси у пользователя 2 числа с помощью промпт и сложить их
+//после этого вывести с помощью алерт "сумма = наше число"
+
+// const firstNum = prompt('firstNum');
+// const secondNum = prompt('secondNum');
+
+// const total = Number(firstNum) + Number(secondNum);
+
+// alert(`сумма = ${total}`);
+
+// let weigh = '88.3';
+// let height = '1,75';
+// const weighSlice = Number(weigh.slice(0, 2));
+// // const weighSliceEnd = +weigh.slice(-1);
+// // const weighSlice = weigh.slice(0, 2);
+// // const weighSliceEnd = weigh.slice(-1);
+// // const newWeigh = weighSlice + '.' + weighSliceEnd;
+// console.log(weighSlice); //slice не меняет
+// // console.log(weighSliceEnd);
+// // console.log(Number(newWeigh));
+// // console.log(+newWeigh);
+
+// // console.log('weigh', Number.parseInt(weigh));
+// // console.log('height', +height);
+
+// // const bmi = +newWeigh / Math.pow(+height, 2);
+// // console.log(bmi);
+// // console.log(bmi.toFixed(2));
+
+// console.log(height.replace(',', '.'));
+
+// console.log(10 >= '7');
+// //когда жс при не строгом сравнении пытается привести к намберу если типы данных разные
+// console.log('2' > '12');
+// console.log('2' < '12');
+// console.log('4' == 4);
+// console.log('6' === 6);
+// //
+// console.log('false' === false); //''false' - строка, true = буль разные типы данных
+
+// console.log(1 == true);
+
+// console.log(1 === true);
+
+// console.log('0' == false);
+
+// console.log('0' === false);
+
+// console.log('0' === false);
+
+//строгое сравнение сначала сравнивает типы а потом значение
+// console.log('Papaya' === 'papaya'); //112>80 поэтому фолс
+// console.log('Papaya'.charCodeAt());
+// console.log('papaya'.charCodeAt());
+
+// console.log(undefined == null);//0==0
+// //undefined -> false -> 0
+// //null -> false -> 0
+// console.log(undefined === null);//разные типы данных
+
+// //unicode
+// console.log('P'.charCodeAt()); //unicode-'P',charCodeAt() 'P' = 80
+// console.log('p'.charCodeAt());
+// console.log('1'.charCodeAt());
+// console.log('2'.charCodeAt());
+// console.log('4'.charCodeAt());
+// console.log('4'.charCodeAt());
+
+//&& возвращает первое false или последнее true, если есть хотя бы одно false вернется false
+// console.log(true && 3);
+// console.log(false && 3);
+// console.log(false && 4 && 'kiwi');
+// console.log(true && 4 && 'kiwi');
+// console.log(true && 0 && 'kiwi');
+// console.log(true && null && 'kiwi');
+
+// //|| возвращает первое true или последнее false
+
+// console.log(true || 3);
+// console.log(true || 3 || 4);
+// console.log(true || false || 7);
+// console.log(-1 || 2 || undefined); //-1 это тру
+// console.log(null || false || undefined);
+
+// console.log((1 && null && 2) > 0);
+// console.log(null || (2 && 3) || 4);
+
+/*?? Значение по умолчанию и оператор нулевого сравнения*/
+/* работает как и || но
+За false принимаются только 2 значения
+ undefined
+ null*/
+
+// console.log(null ?? false ?? null);
+
+// const icon = 0;
+// const img = 10;
+
+// // const mild = icon || img//10
+// const mild = icon ?? img; //0
+// console.log(mild);
+
+// /*%*/
+
+// const totalMinutes = 65;
+// const hours = Math.floor(totalMinutes / 60);
+// const minutes = totalMinutes % 60;
+// // const newHours = String(hours).padStart(2, 0);
+// // const newMinutes = String(minutes).padStart(2, 0);
+// console.log(
+//   `${String(hours).padStart(2, 0)} : ${String(minutes).padStart(2, 0)}`
+// );
+// console.log(`${hours} : ${minutes}`);
+// console.log('hours', hours); //number
+// console.log('hours', newHours); //str
+// console.log('minutes', newMinutes); //str
+
+// console.log('hours', hours);
+// const hours = totalMinutes / 60
+// console.log('hours', hours.toFixed(2));
---
+++ js/mod4.js
@@ -0,0 +1,165 @@
+// function greet(name) {
+//   return `Добро пожаловать ${name}.`;
+// }
+
+// // Вызываем функцию greet и выводим результат в консоль
+// console.log(greet('Манго')); // Добро пожаловать Манго.
+
+// // Выводим функцию greet в консоль не вызывая её
+// console.log(greet); // ƒ greet() { return `Добро пожаловать ${name}.`; }
+
+// // Колбэк-функция
+// function greet(name) {
+//   console.log(`Добро пожаловать ${name}.`);
+// }
+
+// // Функция высшего порядка
+// function registerGuest(name, callback) {
+//   console.log(`Регистрируем гостя ${name}.`);
+//   callback(name);
+// }
+
+// registerGuest("Манго", greet);
+
+// function registerGuest(name, callback) {
+//   console.log(`Регистрируем гостя ${name}.`);
+//   callback(name);
+// }
+
+// // Передаём инлайн функцию greet как колбэк
+// registerGuest('Манго', function greet(name) {
+//   console.log(`Добро пожаловать ${name}.`);
+// });
+
+// // Передаём инлайн функцию notify как колбэк
+// registerGuest('Поли', function notify(name) {
+//   console.log(`Уважаемый(ая) ${name}, ваш номер будет готов через 30 минут.`);
+// });
+
+// function processCall(recipient) {
+//   // Имитируем доступность абонента случайным числом
+//   const isRecipientAvailable = Math.random() > 0.5;
+
+//   if (!isRecipientAvailable) {
+//     console.log(`Абонент ${recipient} недоступен, оставьте сообщение.`);
+//     // Логика активации автоответчика
+//   } else {
+//     console.log(`Соединяем с ${recipient}, ожидайте...`);
+//     // Логика принятия звонка
+//   }
+// }
+
+// processCall('Манго');
+
+
+function processCall(recipient, onAvailable, onNotAvailable) {
+  // Имитируем доступность абонента случайным числом
+  const isRecipientAvailable = Math.random() > 0.5;
+  if (!isRecipientAvailable) {
+    onNotAvailable(recipient);
+    return;
+  }
+  onAvailable(recipient);
+}
+function takeCall(name) {
+  console.log(`Соединяем с ${name}, ожидайте...`);
+  // Логика принятия звонка
+}
+
+function activateAnsweringMachine(name) {
+  console.log(`Абонент ${name} недоступен, оставьте сообщение.`);
+  // Логика активации автоответчика
+}
+
+function leaveHoloMessage(name) {
+  console.log(`Абонент ${name} недоступен, записываем голограмму.`);
+  // Логика записи голограммы
+}
+
+processCall('Манго', takeCall, activateAnsweringMachine);
+processCall('Поли', takeCall, leaveHoloMessage);
+
+
+function printValue(value) {
+  console.log(value);
+}
+
+function prettyPrint(value) {
+  console.log("Logging value: ", value);
+}
+
+function repeat(n, action) {
+  for (let i = 0; i < n; i += 1) {
+    action(i);
+  }
+}
+
+// Передаем printValue как callback-функцию
+repeat(3, printValue);
+// 0
+// 1
+// 2
+
+// Передаем prettyPrint как callback-функцию
+repeat(3, prettyPrint);
+// Logging value: 0
+// Logging value: 1
+// Logging value: 2
+
+// массив.forEach(function callback(element, index, array) {
+//   // Тело коллбек-функции
+// });
+
+// Обычное объявление функции
+function classicAdd(a, b, c) {
+  return a + b + c;
+}
+
+// Тоже самое как стрелочная функция
+const arrowAdd = (a, b, c) => {
+  return a + b + c;
+};
+
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// // Объявление функции
+// numbers.forEach(function (number, index) {
+//   console.log(`Индекс ${index}, значение ${number}`);
+// });
+
+// // Анонимная стрелочная функция
+// numbers.forEach((number, index) => {
+//   console.log(`Индекс ${index}, значение ${number}`);
+// });
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// const logMessage = (number, index) => {
+//   console.log(`Индекс ${index}, значение ${number}`);
+// };
+
+// numbers.forEach(logMessage);
+
+/* Императивный стиль программирования - это такой, который дает машине набор детальных инструкций для выполнения задачи. Например цикл for, который предоставляет точные указания для итерации по индексам массива.*/
+
+// Императивный подход
+const numbers = [1, 2, 3, 4, 5];
+const filteredNumbers = [];
+
+for (let i = 0; i < numbers.length; i += 1) {
+  if (numbers[i] > 3) {
+    filteredNumbers.push(numbers[i]);
+  }
+}
+
+console.log(filteredNumbers); // [4, 5]
+
+/*Декларативное программирование*/
+// Описывает то, что мы хотим получить в результате, а не как это сделать. Порядок выполнения и способ достижения не важен.
+// Декларативное описание задачи более наглядно и легче формулируется. Мы говорим, что хотим сделать, вызвав метод или функцию. Ее реализация, скорее всего использует императивный код, но он скрыт внутри и не усложняет понимание основного кода.
+
+// // Декларативный подход
+// const numbers = [1, 2, 3, 4, 5];
+// const filteredNumbers = numbers.filter(value => value > 3);
+// console.log(filteredNumbers); // [4, 5]

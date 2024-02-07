2247d04bb6b98313eb0a1eff811f197a97e6ee74
Iryna Ielkina <ielkinairinka@gmail.com>
2024-02-07 14:52:20 +0300

    repeat changes to the document

--- js/mod4-01-closures.js
+++ js/mod4-01-closures.js
@@ -1,59 +1,90 @@
+//Замыкание
+
 //Функция результатом своей работы может возвращать другую функцию
 
 //Возвращаем функцию во время вызова будет иметь доступ
 //ко всем локальным переменным (области видимости)
 //родительской функции (той из которой ее вернули)
 //Это называется "Замыканием"
+//Проще говоря если есть функция внутри функции и при возврате вложенной функции получчаем доступ ко всем локальным данным первоначальной функции
 
-const fnA = function (parameter) {
-  const innerVariable = 'Значение внутренней переменной функции fnA';
-  const innerFunction = function () {
+const fnParent = function (parameter) {
+  const parent = 'Значение внутренней переменной функции fnParent';
+
+  const fnChild = function () {
     console.log(parameter);
-    console.log(innerVariable);
-    console.log('Это вызов функции innerFunction');
+    console.log(parent);
+    console.log('Это вызов функции fnChild');
   };
-  return innerFunction;
+
+  return fnChild; //Возвращаемая функция будет иметь доступ ко всем локальным функциям и переменным в теле родительской
+  //т.е функция innerFn видит переменные в своем теле и в переменные в теле родительской fnA
 };
 
-// const fnB = fnA(555);
-// fnB();
-// console.log(fnB);
+//Замыкаем на области видимости fnParent
+const fn = fnParent(555);
+fn();
+
+console.log(fn);
 
 //Поваренок
 
-// const makeDish = function (chiefName, dish) {
-//   console.log(`${chiefName} готовит ${dish}`);
-// };
-
-// makeDish('Mango', 'пирожок');
-// makeDish('Mango', 'омлет');
-// makeDish('Mango', 'чай');
-
-// makeDish('Poly', 'котлеты');
-// makeDish('Poly', 'суп');
-// makeDish('Poly', 'кофе');
-
-const makeChief = function (name) {
-  const makeDish = function (dish) {
-    console.log(`${name} готовит ${dish}`);
-  };
-  return makeDish;
+const makeDish = function (chiefName, dish) {
+  console.log(`${chiefName} готовит ${dish}`);
 };
 
+//при вызове функции повторяется имя шефа это не удобно
+makeDish('Mango', 'пирожок');
+makeDish('Mango', 'омлет');
+makeDish('Mango', 'чай');
+
+makeDish('Poly', 'котлеты');
+makeDish('Poly', 'суп');
+makeDish('Poly', 'кофе');
+
+//корректнее написать функцию которая при вызове не будет передавать каждый раз имя повара
+
+const makeChief = function (name) {
+  // const makeDish = function (dish) {
+  //   console.log(`${name} готовит ${dish}`);
+  // };
+  // return makeDish;
+  //или другой синтаксис
+  return function (dish) {
+    console.log(`${name} готовит ${dish}`);
+  };
+};
 const mango = makeChief('Mango');
-// console.log(mango);
+console.log(mango);
 const poly = makeChief('Poly');
+const katya = makeChief('Катя');
 
 mango('котлеты');
 mango('пирожок');
 poly('котлеты');
 poly('пирожок');
+katya('суп');
 
 /*Округлятор*/
 
 const floatingPoint = 3.456789;
-// const comeInt = Math.round(floatingPoint);
-// const withDecimals = Number(floatingPoint.toFixed(2));
+
+//синтаксис округление
+
+const comeInt = Math.round(floatingPoint);
+console.log(comeInt); //3
+const withDecimals = Number(floatingPoint.toFixed(2));
+console.log(withDecimals); //3.46
+
+//синтаксис округленияс помощью функции
+
+const round = function (number, places) {
+  return Number(number.toFixed(places));
+};
+console.log(round(floatingPoint, 2));
+console.log(round(floatingPoint, 3));
+
+//синтаксис округления с помощью замыкающей функции
 
 function rounder(places) {
   return function (num) {
@@ -66,16 +97,17 @@ const rounder2 = rounder(2);
 const rounder3 = rounder(3);
 
 rounder0(floatingPoint);
-rounder2(floatingPoint);
-rounder3(floatingPoint);
-
 console.log(rounder0(floatingPoint));
+
+rounder2(floatingPoint);
 console.log(rounder2(floatingPoint));
+
+rounder3(floatingPoint);
 console.log(rounder3(floatingPoint));
 
-/*приветные данные и функции - скрытые реализации, интерфейс*/
+/*приватные данные и функции - скрытые реализации, интерфейс*/
 
-const salaryManagerFactory = function (employeeName, baseSalary) {
+const salaryManagerFactory = function (employeeName, baseSalary = 0) {
   let salary = baseSalary;
   // const changeBy = function (amount) {
   //   salary += amount;
@@ -83,23 +115,34 @@ const salaryManagerFactory = function (employeeName, baseSalary) {
   return {
     raise(amount) {
       // changeBy(amount);
-      salary += amount
+      //  salary += amount;
+      //зная методы мы можем изменить данные но в рамках допустимых действий метода
+      if (amount > 1000) {
+        return 'Ты офигел?';
+      }
+      salary += amount;
     },
     lower(amount) {
       // changeBy(amount);
-      salary -= amount
+      salary -= amount;
+      return `Зарплата снижена на ${amount}`
     },
     current() {
-      return `Такая зарплата ${employeeName} - ${salary}`;
+      return `Зарплата ${employeeName} - ${salary}`;
     },
   };
 };
 
-const salaryManager = salaryManagerFactory('mango', 5000)
+//salaryManager обьект return функции salaryManagerFactory
+const salaryManager = salaryManagerFactory('mango', 5000);
+//на прямую поменять данные в обьекте нельзя только через методы
+
 console.log(salaryManager.salary);
 console.log(salaryManager.current());
-salaryManager.lower(1000)
+console.log(salaryManager.raise(5000));
+console.log(salaryManager.lower(1000));
 console.log(salaryManager.current());
+console.dir(salaryManager.raise);
 
 const myLibFactory = function () {
   let value = 0;
@@ -116,8 +159,10 @@ const myLibFactory = function () {
 };
 
 const myLib = myLibFactory();
-
 console.log(myLib);
+
 console.log(myLib.getValue());
 myLib.add(10);
 console.log(myLib.getValue());
+
+

3a3f94d1e398b00dfdddd88694949d5bc15fc813
Iryna Ielkina <ielkinairinka@gmail.com>
2024-02-09 14:06:18 +0300

    repeat. fix

--- js/mod4-02-arrow-fns.js
+++ js/mod4-02-arrow-fns.js
@@ -1,45 +1,80 @@
-/*Стрелочные функции 
-объявление
-явный и не явный возврат
-аргументы
-неявный возврат обьекта*/
+/*- Стрелочные функции 
+- объявление
+- явный и не явный возврат
+- аргументы
+- неявный возврат обьекта*/
 
-// const add = function (a, b, c) {
-//   console.log(a, b, c);
-//   return a + b + c;
-// };
-// const addArrow = (a, b, c) => {
-//    console.log(a, b, c);
-//    return a + b + c;
-// };
-// const addArrow1 = a => {
-//    console.log(a);
-//    return a;
-// };
-// const addArrow0 = (...args) => {
-//    console.log(args);
-//    return;
-// };
+const add = function (a, b, c) {
+  console.log(a, b, c);
+  return a + b + c;
+};
 
-// //
+console.log(add(5, 10, 12));
 
-// console.log(add(5, 10, 12));
-// console.log(addArrow(5, 10, 12));
-// console.log(addArrow1(5));
+//стрелочная фн
+//правила синтаксиса
+//если два и больше аргумента () обязательны
+//если параметр только один то скобки не обязательны
+//если параметров вообще нет тогда обязательны пустые скобки
+//если у стрелочной функции должно быть тело (return, if и т.д)-это явный возврат(где явно указывается return)
+//если в синтаксисе стрелочной функции есть возможность упустить return то это  - не явный возврат
 
-// console.log(addArrow0(5));
+//явный возврат
+const addArrow = (a, b, c) => {
+  return a + b + c;
+};
+console.log(addArrow(5, 10, 12));
 
-// const fnA = function () {
-//   return {
-//     a: 5,
-//   };
-// };
+const addArrow0 = a => {
+  return a;
+};
+console.log(addArrow0(0));
 
-// console.log(fnA());
+//не явный возврат
+const addArrow1 = (a, b, c) => a + b + c;
+console.log(addArrow1(1, 2, 3));
 
-// const arrowFnA = () => ({ arrowA: 5 });
+//arguments нет локальной переменной у стрелочной функции
+const add0 = function (a, b, c) {
+  console.log(arguments);
+  return a + b + c;
+};
+console.log(add0(5, 10, 12));
 
-// console.log(arrowFnA);
+const addArrow2 = (...args) => {
+  console.log(args);
+  // return;
+};
+console.log(addArrow2(50));
+
+/***************************************************** */
+
+const fn = function () {
+  return {
+    a: 5,
+  };
+};
+console.log(fn());
+
+const arrowFn = () => {
+  return {
+    arrowFn: 5,
+  };
+};
+console.log(arrowFn());
+
+//в стрелочных функциях при неявном возврате обьекта фигурные скобки не воспринимаются как возвратом тела обьекта, а воспринимаются как возвратом тела функции
+
+const arrowFnA = () => {
+  arrowFnA: 10;
+};
+console.log(arrowFnA()); //undefined
+
+//для того чтобы из стрелочной функции вернут обьект с неявным возвратом нужно обьект обернуть в круглые скобки и тогда он вернется как обьект
+const arrowFnB = () => ({ arrowFnB: 10 });
+console.log(arrowFnB());
+
+/************************************************************ */
 
 // const onGetPositionSuccess = position => {
 //   console.log(position);
@@ -49,15 +84,15 @@
 // };
 
 // window.navigator.geolocation.getCurrentPosition(
-//   onGetPositionSuccess,
-//   onGetPositionError
-//   //или
-//   // function (position) {
-//   //   console.log(position);
-//   // },
-//   // function (error) {
-//   //   console.log(error);
-//   // }
+//   // onGetPositionSuccess,
+//   // onGetPositionError
+//   // // или инлайново
+//   function (position) {
+//     console.log(position);
+//   },
+//   function (error) {
+//     console.log(error);
+//   }
 // );
 
 /*filter*/
@@ -73,12 +108,15 @@ const filter = function (array, callback) {
   return filteredArray;
 };
 
-// const callback1 =  value => value >= 3;
-// const callback2 =  value => value <= 3;
-// const getFruitsQuantity =  fruits=> fruits.quantity >= 120;
+const callback1 = value => value >= 3;
+
+const callback2 = value => value <= 3;
+
+const getFruitsQuantity = fruits => fruits.quantity >= 120;
 
 const r1 = filter([1, 2, 3, 4, 5], value => value >= 3);
 console.log(r1);
+
 const r2 = filter([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], value => value <= 3);
 console.log(r2);
 
@@ -91,22 +129,57 @@ const fruits = [
 const r3 = filter(fruits, fruits => fruits.quantity >= 120);
 console.log(r3);
 
-//синтаксис
+/*синтаксис
 //Синтаксис стрелочной функции
 //с параметрами
 //с одним параметром
-//без параметров
+//без параметров*/
 
-//явный и неявный возврат
-// const add = function (a, b) {
-//   return a + b;
-// };
-
-//стрелочная
-const add = (a, b) => a + b; //неявный возврат
-const abb = (a, b) => {
+//разница  в синтаксисе
+const fn1 = function (a, b) {
   return a + b;
 };
+const fn2 = (a, b) => {
+  return a + b;
+};
+
+console.log(fn1(1, 2));
+console.log(fn2(1, 2));
+
+//если параметр один
+//Убираются круглые скобки
+
+const fn3 = message => {
+  return message;
+};
+console.log(fn3('Hi'));
+
+//Если параметров нет
+const fn4 = () => {
+  console.log('hi');
+};
+fn4();
+
+//явный и неявный возврат
+const add1 = function (a, b) {
+  return a + b;
+};
+
+//явный(explicit) возврат (есть тело функции и return)
+const add2 = (a, b) => {
+  return a + b; //тело функции
+};
+
+//не явный(implicit) возврат
+//ели нет в теле функции дополнительных условий выполнения функции а только return
+const add3 = (a, b) => a + b;
+
+//стрелочная
+const add4 = (a, b) => a + b; //неявный возврат
+const add5 = (a, b) => {
+  return a + b; //тело функции
+};
+
 //явный возврат если есть тело
 const acc = message => {
   return message;
@@ -115,71 +188,79 @@ const att = () => {
   return 'hi';
 };
 
-console.log(abb(1, 2));
-console.log(acc('hi'));
-console.log(att());
-
 //Псевдомассив и arguments
+// const ass = () => {
+//   console.log(arguments);//arguments в стрелках не существует
+// };
+// ass(1, 2, 3);//error
 
-const ass = (...args) => {
+const ass1 = (...args) => {
+  //(...args) скобки обязательны
   console.log(args);
 };
-ass(2, 3, 4, 5, 6, 7);
+ass1(2, 3, 4, 5, 6, 7);
 
 //context
-
 //⁡⁣⁣⁢Контекст стрелки⁡
-
 //Контекст внутри стрелки определяется местом ее объявления, а не вызова,
 //и ссылается на контекст родительской функции
+//у стрелки нет своего this, определяется не в момент вызова а в момент обьявления, т.е в стрелке
+const showThis = () => {
+  console.log('This is showThis: ', this);
+};
 
-// const showThis = () => {
-//   console.log('This is showThis: ', this);
+showThis();
 
-// }
+const user = { name: 'Mango' };
+user.showContext = showThis;
 
-// showThis();
-
-// const user = { name: 'Mango' }
-// user.showContext = showThis;
-
-// user.showContext();
+user.showContext();
 
 //Стрелка как методы обьекта
-
-// const user = {
-//   fullName: 'Mango',
-//   showName() {
-//     console.log('this: ', this);
-//     console.log('this.fullName: ', this.fullName);
-//     const inner = () => {
-//       console.log('this in inner', this);
-//     };
-//     inner();
-//   },
-// };
-// user.showName();
-
-const user = {
+const user1 = {
   fullName: 'Mango',
   showName() {
     console.log('this: ', this);
     console.log('this.fullName: ', this.fullName);
+    //вызов inner без контекста в обычной фн будет undefine (this -> inner)
+    const inner = function () {
+      console.log('this in inner', this);
+    };
+    inner();
+    //а в стрелке объявленной внутри метода обьекта, который вызывается в контексте user получит контекст функции метода (this -> showName)
+    const inner1 = () => {
+      console.log('this in inner', this);
+    };
+    inner1();
   },
 };
+user1.showName();
 
-user.showName();
+//❌Никогда не используйте стрелки как методы обьекта
+// const user2 = {
+//   fullName: 'Mango',
+//   // showName() {
+//   //   console.log('this: ', this);
+//   //   console.log('this.fullName: ', this.fullName);
+//   // },
+//   showName () => {
+//     console.log('this: ', this);
+//     console.log('this.fullName: ', this.fullName);
+//   },//error
+// };
+// user2.showName();
 
-//стрелка -конструктор  не может быть
-
-// const User = name => {
+//❌стрелка -конструктор  не может быть
+const User = function (name) {
+  this.name = name;
+};
+// const User = name => { //error
 //   this.name = name
 // }
 
-// console.log(new User('Mango'));
+console.log(new User('Mango'));
 
-//стрелка как метод обьекта
-
+//❌стрелка как метод обьекта
 const objA = {
   x: 5,
   showX() {
@@ -187,79 +268,86 @@ const objA = {
     console.log(this.x);
     const objB = {
       y: 10,
-      showThis: () => {
-        //стрелочные функции как метод обьекта безполезны
-        console.log('this objB', this);
+      showThis() {
+        console.log('this showThis', this);//ссылается на обьект objB
+      },
+      //стрелочные функции как метод обьекта бесполезны. this в стрелке из внешней области видимости 
+      //и будет ссылаться не на локальный обьект а на внешний 
+      showThisArrow: () => {
+        console.log('this showThisArrow', this);//ссылается обьект objA
       },
     };
     objB.showThis();
+    objB.showThisArrow();
   },
 };
 objA.showX();
 
-// ⁡⁣⁣⁢рефакторинг
-
+//⁢рефакторинг
 //цепочка вызовов - chaining
 
 const numbers = [1, 2, 3, 4, 5, 6];
+
 //было
-// const greaterThenTwo = numbers.filter(function (num) {
-//   return num > 2;
-// });
-//рефакторинг стало
-const greaterThenTwo = numbers.filter(num => num > 2);
+const greaterThenTwo = numbers.filter(function (num) {
+  return num > 2;
+});
 console.log(greaterThenTwo);
+//стало
+const greaterThenTwo1 = numbers.filter(num => num > 2);
+console.log(greaterThenTwo1);
 
 //было
-// const multByTwo = greaterThenTwo.map(function (num) {
-//   return num * 3;
-// });
-//стало
-const multByTwo = greaterThenTwo.map(num => num * 3);
+const multByTwo = greaterThenTwo.map(function (num) {
+  return num * 3;
+});
 console.log(multByTwo);
+//стало
+const multByTwo1 = greaterThenTwo.map(num => num * 3);
+console.log(multByTwo1);
 
 //было
-// const sorted = multByTwo.sort(function (a, b) {
-//   return a - b;
-// });
-//стало
-const sorted = multByTwo.sort((a, b) => a - b);
+const sorted = multByTwo.sort(function (a, b) {
+  return a - b;
+});
 console.log(sorted);
+//стало
+const sorted1 = multByTwo.sort((a, b) => a - b);
+console.log(sorted1);
 
 //было
-// const rest = numbers
-//   .filter(function (num) {
-//     return num > 2;
-//   })
-//   .map(function (num) {
-//     return num * 3;
-//   })
-//   .sort(function (a, b) {
-//     return a - b;
-//   });
-//стало
 const rest = numbers
+  .filter(function (num) {
+    return num > 2;
+  })
+  .map(function (num) {
+    return num * 3;
+  })
+  .sort(function (a, b) {
+    return a - b;
+  });
+//стало
+const rest1 = numbers
   .filter(num => num > 2)
   .map(num => num * 3)
   .sort((a, b) => a - b);
-console.log(sorted);
 
-//
-
-// const players = [
-//   { id: 'id-1', tag: 'Mango', isOnline: true, rank: 800 },
-//   { id: 'id-2', tag: 'Poly', isOnline: false, rank: 600 },
-//   { id: 'id-3', tag: 'Ajax', isOnline: true, rank: 100 },
-//   { id: 'id-4', tag: 'Kiwi', isOnline: true, rank: 400 },
-// ];
-
-// const onlineAndSorted = players
-//   .filter(player => player.isOnline)
-//   .sort((prevPlayer, nextPlayer) => prevPlayer.rank - nextPlayer.rank);
-
-// console.log(onlineAndSorted);
+/************************************************************************ */
 
 const players = [
+  { id: 'id-1', tag: 'Mango', isOnline: true, rank: 800 },
+  { id: 'id-2', tag: 'Poly', isOnline: false, rank: 600 },
+  { id: 'id-3', tag: 'Ajax', isOnline: true, rank: 100 },
+  { id: 'id-4', tag: 'Kiwi', isOnline: true, rank: 400 },
+];
+
+const onlineAndSorted = players
+  .filter(player => player.isOnline)
+  .sort((prevPlayer, nextPlayer) => prevPlayer.rank - nextPlayer.rank);
+
+console.log(onlineAndSorted);
+
+const players1 = [
   { id: 'player-1', timePlayed: 310, points: 54, name: 'Mango', online: false },
   { id: 'player-2', timePlayed: 470, points: 92, name: 'Poly', online: true },
   { id: 'player-3', timePlayed: 230, points: 48, name: 'Ajax', online: true },
@@ -268,36 +356,37 @@ const players = [
 ];
 //увеличить количество поинтов каждого игрока
 //было
-// const updatedPlayers = players.map(function (player) {
-//   return {
-//     ...player,
-//     points: player.points + player.points * 0.1,
-//   };
-// });
-
+const updatedPlayers = players1.map(function (player) {
+  return {
+    ...player,
+    points: player.points + player.points * 0.1,
+  };
+});
+console.table(updatedPlayers);
 //стало
-// const updatedPlayers = players.map(player => ({
-//   ...player,
-//   points: player.points + player.points * 0.1,
-// }));
+const updatedPlayers1 = players1.map(player => ({
+  ...player,
+  points: player.points + player.points * 0.1,
+}));
 
-// console.table(updatedPlayers);
+console.table(updatedPlayers);
 
 //увлечение кол-во часов игрока по id
 
 const playerIdToUpdate = 'player-3';
 //было
-// const updatedPlayers = players.map(function (player) {
-//   if (player.id === playerIdToUpdate) {
-//     return {
-// ...player,
-//       timePlayed: player.timePlayed + 50,
-//     };
-//   }
-//   return player;
-// });
+const updatedPlayers3 = players.map(function (player) {
+  if (player.id === playerIdToUpdate) {
+    return {
+      ...player,
+      timePlayed: player.timePlayed + 50,
+    };
+  }
+  return player;
+});
+console.log(updatedPlayers);
 //стало
-const updatedPlayers = players.map(player =>
+const updatedPlayers4 = players.map(player =>
   player.id === playerIdToUpdate
     ? { ...player, timePlayed: player.timePlayed + 50 }
     : player

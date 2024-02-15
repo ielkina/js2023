a9299308db03fb86cf6ce4dacb3d05548e201c17
Iryna Ielkina <ielkinairinka@gmail.com>
2024-02-15 17:51:43 +0300

    repiad

--- js/mod5-06-counter.js
+++ js/mod5-06-counter.js
@@ -11,7 +11,7 @@ const CounterPlugin = function ({
   this._step = step; //_step - приватное свойство
   this._rootSelector = rootSelector;
   this._value = initialValue;
-  this._refs = this._getRef(rootSelector);//передаем в конструктор, вызываем, и получаем ссылку в метод ссылок
+  this._refs = this._getRef(rootSelector); //передаем в конструктор, вызываем, и получаем ссылку в метод ссылок
   this.onUpdate = onUpdate;
   this._bindEvents();
   this.updateValueUI();
@@ -28,8 +28,8 @@ CounterPlugin.prototype.decrement = function () {
 
 //получаем ссылки на элементы
 CounterPlugin.prototype._getRef = function (rootSelector) {
-  const refs = {};//создаем элемент 
-  refs.container = document.querySelector(rootSelector);//получаем все ссылки из элемента
+  const refs = {}; //создаем элемент
+  refs.container = document.querySelector(rootSelector); //получаем все ссылки из элемента
   // console.log(`container`, refs.container)
   refs.incrementBtn = refs.container.querySelector('[data-increment]');
   // console.log(` refs.incrementBtn`,  refs.incrementBtn)
@@ -37,10 +37,10 @@ CounterPlugin.prototype._getRef = function (rootSelector) {
   // console.log(`refs.decrementBtn`, refs.decrementBtn)
   refs.value = refs.container.querySelector('[data-value]');
   // console.log(` refs.value`,  refs.value)
-  return refs;//и возвращаем их
+  return refs; //и возвращаем их
 };
 
-//добавляем событие на элементы 
+//добавляем событие на элементы
 CounterPlugin.prototype._bindEvents = function () {
   this._refs.incrementBtn.addEventListener('click', () => {
     console.log('CounterPlugin.prototype.bindEvent -> this', this);
--- js/mod5-07-class.js
+++ js/mod5-07-class.js
@@ -1,5 +1,6 @@
-//old sckool
+//old school синтаксиса класса
 const Car = function ({ brand, model, price } = {}) {
+  //конструктор
   this.brand = brand;
   this.model = model;
   this.price = price;
@@ -31,98 +32,123 @@ const car = new Car({
  * - синтаксис публичных свойств и методов классов
  * - геттеры и сеттеры
  */
-//классы
+
+/*Классы*/
+
 class Car1 {
-  //статическое свойство
-  static aaa = 'aaa';
+  // порядок важен - сначала статические ➡ конструктор ➡ методы
+  // статическое свойство начинаются со слова static и в конце ;
+  static description = 'Класс описывающий автомобиль';
   //статический метод
-  //порядок важен - сначала статические потом конструктор потом методы
   static logInfo(carObj) {
     console.log('Car.logInfo  -> carObj', carObj);
   }
   //приватное свойство пишется через #,из вне их нельзя но можно использовать в методах класса(стр54)
   //приватные свойства идут на экземпляр, статические на конструкцию
+  //приватное свойство бывает не у экземпляра а у класса (в конструкторе его не прописывают)
+  //его никак не причитать и не записать из вне, оно доступно только в методах класса
   #test = 'test';
-
   //синтаксис публичного свойства
+  //все что объявляем публичным свойством идет на экземпляр
   mySuperPublicField = 555;
-
+  //конструктор
   constructor({ brand, model, price } = {}) {
     console.log('Выполняется constructor');
     console.log(this);
-    this.brand = brand;
+    this._brand = brand;
     this._model = model;
     this._price = price;
-
     //тоже самое что и код выше
     this.mySuperPublicField = 555;
+  }
+
+  //метод класса
+
+  //публичный метод хранится не на прототипе а в экземпляре, так лучше не делать
+  changePrice(newPrice) {
+    this.price = newPrice;
+    console.log(this.#test);//доступ к значению приватного свойства 
+  }
+
+  updateModel = newModel => {
+    this.model = newModel;
+  };
+
+  // getter получит
+  getModel() {
+    return this.model;
+  }
+  // setter изменение
+  setModel(newModel) {
+    this.model = newModel;
+  }
+
+  //вместо этих двух свойств можно сделать гетеры и сетеры
+
+  //гетер или сетер не могут называться также как и свойство c которое они работают
+  //поэтому в конструкторе лучше писать свойство чез нижнее подчеркивание иначе будет ошибка
+  //get set (функции) попадают на каждый экземпляр
+  // getter получит
+  get model() {
+    return this._model
+  }
+  // setter изменение
+  set model(newModel) {
+    return this._model = newModel
   }
 
   get price() {
     return this._price;
   }
+
   set price(newPrice) {
     this._price = newPrice;
   }
-  // changePrice(newPrice) {
-  //   this.price = newPrice;
-  //   console.log(this.#test);
-  // }
-  //публичный метод хранится не на прототипе а в экземпляре, так лучше не делать
-  updateModel = newModel => {
-    this.model = newModel;
-  };
-
-  //гетер или сетер не могут называться также как и свойство c которое они работают
-  //setter
-  // setModel(newModel) {
-  //   this.model = newModel;
-  // }
-
-  //getter получит
-  // getModel() {
-  //   return this.model;
-  // }
-
-  //фн геттера
-  get model() {
-    return this._model;
-  }
-
-  set model(newModel) {
-    this._model = newModel;
-  }
 }
-
 console.dir(Car1);
+
+console.log(Car1.description);
 
 const carInstance = new Car1({
   brand: 'Audi',
   model: 'Q3',
   price: 35000,
 });
+//прототип класса
+console.log(Object.getPrototypeOf(carInstance) === Car1.prototype);
+//экземпляр класса
+console.log(carInstance);
+//метод класса
+Car1.logInfo(carInstance);
+//приватное свойство класса
+// console.log(carInstance.#test)//SyntaxError
+// console.log(carInstance.test)//undefined
 
-console.log(carInstance.model); //
+//доступ к значению приватного свойства
+carInstance.changePrice(2000)
 
+
+//изменение с помощью методов класса
+carInstance.changePrice(2000);
+console.log(carInstance.price);
+carInstance.updateModel('BMW')
+console.log(carInstance.model);
+console.log(carInstance.getModel());
+carInstance.setModel('Q4')
+console.log(carInstance.getModel())
+carInstance.setModel('Q5');
+console.log(carInstance.model);
+
+
+//изменение с помощью геттеров и сетеров 
 carInstance.model = 'Q4'; //запись значения setter
-
 console.log(carInstance.model); //получение значение getter
-
 console.log(carInstance.price)
-
 carInstance.price = 600000
-
 console.log(carInstance.price)
+console.log('carInstance', carInstance);
 
-// console.log(Object.getPrototypeOf(carInstance) === Car1.prototype);//true
 
-// console.log('carInstance', carInstance);
 
-// Car1.logInfo(carInstance);
 
-// carInstance.changePrice(2000);
 
-// console.log(carInstance.getModel());
-
-// carInstance.setModel('Q5');
-// console.log(carInstance.getModel());

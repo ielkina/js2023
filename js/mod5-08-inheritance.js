7257cf3b29e75caa5aa987095229b1e1ba5c8b7b
Iryna Ielkina <ielkinairinka@gmail.com>
2024-02-15 22:30:57 +0300

    inheritance pepiad

--- js/mod5-08-inheritance.js
+++ js/mod5-08-inheritance.js
@@ -1,8 +1,11 @@
 /**
- * Наследование
+ * - Наследование
  * - extends
  * - super()
  */
+
+//некоторые свойства в классах дублируются и это плохо
+//пример
 // class Warrior {
 //   constructor() {
 //     this.name = '';
@@ -24,14 +27,17 @@ class Hero {
   constructor({ name = 'hero', xp = 0 } = {}) {
     this.name = name;
     this.xp = xp;
+    console.log(this)
   }
-  gainXp(amount) {
-    console.log(`${this.name} получает ${amount} опыта`);
-    this.xp += amount;
+
+  gainXp(value) {
+    console.log(`${this.name} получает ${value} опыта`);
+    this.xp += value;
   }
 }
 
-//наследование
+/*наследование*/
+
 class Warrior extends Hero {
   constructor({ weapon, ...restProps } = {}) {
     //super фн конструктора родительского класса, когда он вызывается тогда this будет доступен в наследуемом классе
@@ -44,24 +50,18 @@ class Warrior extends Hero {
   }
 }
 
-// class Berserk extends Warrior{
-//   constructor({ ...restProps } = {}) {
-//     super(restProps);
-//     this.warcry =
-//    }
-// }
-
 class Berserk extends Warrior {
   constructor({ warcry, ...restProps } = {}) {
     super(restProps);
     this.warcry = warcry;
   }
-  babyRage() {
-    console.log(`${this.name} кричит ${this.warcry}`);
+  babyRage(warcry) {
+    console.log(`${this.name} кричит ${warcry || this.warcry}`);
   }
 }
 
 class Mage extends Hero {
+
   constructor({ spells, ...restProps } = {}) {
     super(restProps);
     this.spells = spells;
@@ -71,15 +71,25 @@ class Mage extends Hero {
   }
 }
 
-const mango = new Warrior({ name: 'mango', xp: 1000, weapon: 'алебарда' });
+const mango = new Hero({ name: 'mango', xp: 100 }); //Hero {name: 'mango', xp: 100}
+mango.gainXp(1000); //добавляем опыт с помощью метода
 console.log(mango);
-mango.attack();
-mango.gainXp(1000);
+
+//при вызове new Warrior вызывается конструктор класса Warrior
+//свойства обьекта new Warrior передаются в параметры конструктора класса Warrior
+//в конструкторе вызывается фн super
+//Super это вызов fn конструктора класса Hero
+//при вызове super Параметры конструктора Warrior передаются в конструктор наследуемого класса те Hero
+const sara = new Warrior({ name: 'sara', xp: 200, weapon: 'нож' }); //Hero {name: 'mango', xp: 100}
+sara.gainXp(1000); //добавляем опыт с помощью метода
+console.log(sara);
+sara.attack();
 
 const poly = new Mage({ name: 'poly', xp: 5000, spells: ['фаербол'] });
 console.log(poly);
 poly.cast();
 poly.gainXp(10);
+
 
 const kiwi = new Berserk({
   name: 'kiwi',
@@ -87,34 +97,44 @@ const kiwi = new Berserk({
   weapon: 'knife',
   warcry: 'Yahooo',
 });
-kiwi.babyRage();
+kiwi.babyRage('jgkhgkjhgkhjg');
 kiwi.attack()
 kiwi.gainXp(50)
 
+
+//цепочка наследования
+console.log(mango.__proto__ === Hero.prototype);
+console.log(sara.__proto__ === Warrior.prototype);
+console.log(poly.__proto__ === Mage.prototype);
+console.log(kiwi.__proto__ === Berserk.prototype);
+console.log(Object.getPrototypeOf(sara) === Warrior.prototype);
+console.log(Hero.prototype.__proto__ === Object.prototype);
+console.log(Warrior.prototype.__proto__ === Hero.prototype);
+console.log(Berserk.prototype.__proto__ === Warrior.prototype);
+console.log(Berserk.prototype.__proto__ === Hero.prototype);
+console.log(Mage.prototype.__proto__ === Warrior.prototype);
+console.log(Mage.prototype.__proto__ === Hero.prototype);
 console.log('Warrior.prototype', Warrior.prototype);
 console.log('Hero.prototype', Hero.prototype);
 
-// //цепочка наследования
-
-console.log(mango.__proto__ === Warrior.prototype);
-console.log(Object.getPrototypeOf(mango) === Warrior.prototype);
-console.log('Warrior.prototype', Warrior.prototype);
-console.log(Warrior.prototype.__proto__ === Hero.prototype);
-console.log(Hero.prototype.__proto__ === Object.prototype);
-
-// const mango = new Hero({ name: 'mango', xp: 100 });
-// console.log(mango);
-// mango.gainXp(1000);
 
 //JS не строго типизированный язык т.е в переменные могут быть записаны разные значения
 //в JS у переменной нет значения, значение есть у типа
 
 let name = 'mango';
-
 name = true;
 name = [];
 name = 5;
+console.log(name);// 5 тип данных для JS не имеет значение 
 
 //TS строго типизированный язык
-
 //TS playground
+// https://www.typescriptlang.org/play?ssl=3&ssc=13&pln=1&pc=1#code/DYUwLgBArgziBOA5AhgWxBAvBA5K5AdgOYD2OAULAiulrgA4nACeFVSaG2ArEA
+let userName = 'mango'
+userName = 'poly'
+userName = 5 //ошибка из за разного типа данных у переменной
+
+
+//Знать обязательно
+
+console.log([1, 2, 3, 4, 5].map(x => x * 2).filter(y => y < 5));

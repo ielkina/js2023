a092c9a3d4f9c349c90056306e9a0ec6d88d266c
ielkina <ielkinairinka@gmail.com>
2024-06-15 21:29:16 +0300

    deleted src, mod .gitignore

--- src/js/validate-password.js
+++
@@ -1,9 +0,0 @@
-//абсолютный импорт (библиотека)
-import Joi from 'joi';
-console.log(Joi);
-
-export default function validatePassword(password) {
-  return true;
-}
-
-//экспортировать можно только что то одно (функция)
